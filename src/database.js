58c9fe6f856d7acd3eea2cb94dbfedcaca377012
Anderson Mosquera <anderson.mosquera9610@gmail.com>
2018-12-21 21:02:03 -0500

    change database to connect in mlab

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-stack-tasks';
+const URI = 'mongodb://anderson1096:root123@ds157702.mlab.com:57702/mern-stack-tasks';
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then( db => console.log('DB is connected'))
